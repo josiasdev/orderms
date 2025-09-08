eaa7e2ad8b08557f1fd023c94725fc38d0764442
josiasdev <francisco.batista67@alu.ufc.br>
2025-09-08 13:07:34 -0300

    feat: Criando ordem para cada item de produtos.

---
+++ src/main/java/com/github/josiasdev/orderms/entity/OrderItem.java
@@ -0,0 +1,43 @@
+package com.github.josiasdev.orderms.entity;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.math.BigDecimal;
+
+
+public class OrderItem {
+    private String product;
+    private Integer quantity;
+
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal price;
+
+    public OrderItem(){
+
+    }
+
+    public void setProduct(String product){
+        this.product = product;
+    }
+
+    public void setQuantity(Integer quantity){
+        this.quantity = quantity;
+    }
+
+    public void setPrice(BigDecimal price){
+        this.price = price;
+    }
+
+    public String getProduct(){
+        return product;
+    }
+
+    public Integer getQuantity(){
+        return quantity;
+    }
+
+    public BigDecimal getPrice(){
+        return price;
+    }
+}
